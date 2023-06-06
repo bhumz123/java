20d3471ea7a0be3df39861b93a5c9c0008df8e98
bhumz123 <bhumu983@gmail.com>
2023-06-06 20:52:31 +0530

    fold

---
+++ operators/op.java
@@ -0,0 +1,13 @@
+// symbols that tell compiler to perfom some operation
+// operation is performed on two operators
+// we have different types of operators
+// binary: + , - , * , / , %- gives remainder
+// unary ++ , --
+
+
+
+package operators;
+
+public class op {
+    
+}
