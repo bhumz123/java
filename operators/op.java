02d6046d06a2f4a2b5bcd577076894dcba0e4cfd
bhumz123 <bhumu983@gmail.com>
2023-06-06 21:24:30 +0530

    add

--- operators/op.java
+++ operators/op.java
@@ -2,12 +2,35 @@
 // operation is performed on two operators
 // we have different types of operators
 // binary: + , - , * , / , %- gives remainder
-
-
-
+// unary ++ , --
+// pre increment - ++a, first change value then use
+// post increment - a++, first use value then change
+// same for decrement
 
 package operators;
 
+import javax.sound.sampled.SourceDataLine;
+
 public class op {
+    public static void main(String[] args) {
+        
+    
+    // post increement
+    // value assign then change
+    int a = 10;
+    int b=0;
+    b=a++;
+    System.out.println(a); // gives 11
+    System.out.println(b); // gives
+
+    // pre increment
+    // value change then assign
+    int c = 10;
+    int d = 0;
+    d = ++c;
+    System.out.println(c); // gives 11
+    System.out.println(d); // gives 11
+
+    }
     
 }
---
+++ operators/rltn.java
@@ -0,0 +1,24 @@
+// relational operators
+// == , != , > , < , >= , <=
+// the output is always a boolean value , ie true or false
+
+// logical operator
+//  ||- and , && - or, ! - not
+
+package operators;
+
+public class rltn {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+        int c = 50;
+
+        if (a >b || a>c ){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+    }
+    
+}
